2d93f1e9779cf4a2b98ddc08a5c60a823ac5812f
LearnerQG <mdabdulmuiz97@gmail.com>
2021-11-04 20:34:37 -0700

    ASD

--- routes/authors.js
+++ routes/authors.js
@@ -3,22 +3,27 @@ const router = express.Router()
 const Author = require('../models/authorcopy.js')
 
 // All author route
-router.get('/redo',async (req,res)=>{
-    let searchOptions = {}
-    if (req.query.name3 != null && req.query.name3 !== '') {
-       searchOptions.name = new RegExp(req.query.name3, 'i')
-    }
-    try{
-        const authors = await Author.find(searchOptions)
-        res.render('authors/redo.ejs', {
-            authors: authors,
-             searchOptions: req.query
-        } )
-    }
-    catch{
-        res.redirect('/')
-    }
+// router.get('/',async (req,res)=>{
+//     let searchOptions = {}
+//     if (req.query.name3 != null && req.query.name3 !== '') {
+//        searchOptions.name = new RegExp(req.query.name3, 'i')
+//     }
+//     try{
+//         const authors = await Author.find(searchOptions)
+//         res.render('authors/index.ejs', {
+//             authors: authors,
+//              searchOptions: req.query
+//         } )
+//     }
+//     catch{
+//         res.redirect('/')
+//     }
     
+// })
+
+// experiment author route
+router.get('/', (req,res)=>{
+    res.render('authors/index.ejs'/*, {author: new Author() }*/)
 })
 
 // New author route
