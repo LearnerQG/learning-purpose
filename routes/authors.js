4ef174435ced19b1b81a988a0265f2cb88f0f388
LearnerQG <mdabdulmuiz97@gmail.com>
2021-11-04 19:51:09 -0700

    ASD

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const Author = require('../models/author')
+const Author = require('../models/authorcopy.js')
 
 // All author route
-router.get('/',async (req,res)=>{
+router.get('/redo',async (req,res)=>{
     let searchOptions = {}
     if (req.query.name3 != null && req.query.name3 !== '') {
        searchOptions.name = new RegExp(req.query.name3, 'i')
     }
     try{
         const authors = await Author.find(searchOptions)
-        res.render('authors/index.ejs', {
+        res.render('authors/redo.ejs', {
             authors: authors,
              searchOptions: req.query
         } )
@@ -23,13 +23,13 @@ router.get('/',async (req,res)=>{
 
 // New author route
 router.get('/new', (req,res)=>{
-    res.render('authors/new.ejs', {author: new Author() })
+    res.render('authors/new.ejs'/*, {author: new Author() }*/)
 })
 
 // create author route
 router.post('/',async (req,res)=>{
     const author = new Author({
-        name1: req.body.name2
+        name: req.body.name2
     })
 try{
 const newAuthor = await author.save()
